1007d5668799a3661fefd4d42e231ab9a11613ba
Anirudh joshi <141419375+Anirudhj0shi@users.noreply.github.com>
2023-12-11 22:32:52 +0530

    Update ParentComponent.jsx

--- REACT_test/src/ParentComponent.jsx
+++ REACT_test/src/ParentComponent.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ChildComponent from './ChildComponent';
 function ParentComponent() {
-  // Sample array of items passed as props to ChildComponent
   const items = [
     { id: 1, name: 'Item 1', category: 'A' },
     { id: 2, name: 'Item 2', category: 'B' },
